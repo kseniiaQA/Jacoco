065f13372a0a03e9f8669327c9daaa7adc5ad778
Your Name <you@example.com>
2021-06-03 10:07:55 +0300

    test coverage 100%

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -1,2 +1,32 @@
-public class StatisticsServiceTest  {
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    {
+
+    StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {8, 6, 11, 11, 12, 12, 5, 8, 4, 5, 3,};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+
+    }
 }
